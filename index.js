0da067a9d7be1c2b78f92348cde656dd25fe6125
Farhan Hussain <princeofficial355@gmail.com>
2024-07-22 19:14:00 +0530

    message

--- index.js
+++ index.js
@@ -9,31 +9,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+// "/" Routing
 app.get("/", (req, res) => {
   fs.readdir(`./files`, (err, files) => {
     res.render("index", { files: files });
   });
 });
 
+
+// create file
 app.post("/create", (req, res) => {
   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,`${req.body.details}`,(err) => {
       res.redirect("/");
     });
 });
 
-
-app.post("/update", (req, res) => {
-  fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, (err)=>{
-    res.redirect("/")
-  })
-});
-
-// dynamic routing
-
-// app.get("/profile/:username", (req, res)=>{
-//   res.send(`Hello ,${req.params.username}`)
-// })
-
+//read file
 
 app.get("/file/:filename", (req, res)=>{
   fs.readFile(`./files/${req.params.filename}`, "utf-8", (err , filedata)=>{
@@ -41,11 +32,30 @@ app.get("/file/:filename", (req, res)=>{
   })
 })
 
+// Updation of file------------------------------------------------>
+
+app.post("/update", (req, res) => {
+  fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, (err)=>{
+    res.redirect("/")
+  })
+});
+
+
 app.get("/edit/:filename", (req, res)=>{
   res.render("edit", {filename : req.params.filename})
 })
 
+//Deletion of a file-------------------------------------------------->
 
+app.get(`/delete/:filename`,(req, res)=>{
+  fs.rm(`./files/${req.params.filename}`, (err)=>{
+    res.redirect("/")
+  })
+})
+
+
+
+// listening port----------------------------------------------------->
 app.listen(3000, () => {
   console.log(`App is running on the port localhost:${3000}`);
 });
